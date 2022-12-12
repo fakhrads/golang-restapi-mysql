d7f8f7dc26fc559578ea905cccee4885c673f334
fakhrads <fakhriadisaputra15@gmail.com>
2022-12-12 15:07:37 +0700

    add controller

---
+++ controller/book.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"net/http"
+
+	"project/rest_api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+// GET /books
+// Get all books
+func FindBooks(c *gin.Context) {
+	var books []models.Book
+	models.DB.Find(&books)
+
+	c.JSON(http.StatusOK, gin.H{"data": books})
+}
--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"project/rest_api/controller"
+	"project/rest_api/models"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	models.ConnectDatabase()
+
+	r.GET("/books", controllers.FindBooks)
 
 	r.Run()
 }
