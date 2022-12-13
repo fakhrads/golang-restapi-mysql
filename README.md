2f9435e9d4c9f7525b38c067f9dccca76746ff16
fakhrads <fakhriadisaputra15@gmail.com>
2022-12-13 18:01:24 +0700

    Update

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## Golang with Gin and RESTAPI
 
-I create this for preparing my project with microservices architecture. 
-
-NextJS with Golang and MySQL is enough. I thougt
+Golang with Gin
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"project/rest_api/controller"
+	controllers "project/rest_api/controller"
 	"project/rest_api/models"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +13,10 @@ func main() {
 	models.ConnectDatabase()
 
 	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
 
 	r.Run()
 }
